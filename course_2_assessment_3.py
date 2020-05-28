43a7a01adec620afeb1599b466e0dff62c1140cc
shawonAlam <30968890+shawonAlam@users.noreply.github.com>
2020-05-28 07:45:53 +0600

    Update course_2_assessment_3.py

--- course_2_assessment_3.py
+++ course_2_assessment_3.py
@@ -1,4 +1,4 @@
-1.The dictionary Junior shows a schedule for a junior year semester. The key is the course name and the value is the number of credits. Find the total number of credits taken this semester and assign it to the variable credits. Do not hardcode this – use dictionary accumulation!
+# 1.The dictionary Junior shows a schedule for a junior year semester. The key is the course name and the value is the number of credits. Find the total number of credits taken this semester and assign it to the variable credits. Do not hardcode this – use dictionary accumulation!
 
 Junior = {'SI 206':4, 'SI 310':4, 'BL 300':3, 'TO 313':3, 'BCOM 350':1, 'MO 300':3}
 credits = 0
@@ -7,7 +7,7 @@ for key in Junior:
     
 print(credits)
 
-2.Create a dictionary, freq, that displays each character in string str1 as the key and its frequency as the value.
+# 2.Create a dictionary, freq, that displays each character in string str1 as the key and its frequency as the value.
 
 
 str1 = "peter piper picked a peck of pickled peppers"
@@ -20,10 +20,11 @@ for char in str1:
 
 for key in freq:
     print("'", key, "'", " character showed ", freq[key], " times." )
+    
 
 
 
-3.Provided is a string saved to the variable name s1. Create a dictionary named counts that contains each letter in s1 and the number of times it occurs.
+# 3.Provided is a string saved to the variable name s1. Create a dictionary named counts that contains each letter in s1 and the number of times it occurs.
 
 s1 = "hello"
 
@@ -35,7 +36,7 @@ for ch in s1:
     counts[ch] += 1
 print(counts)
 
-4.Create a dictionary, freq_words, that contains each word in string str1 as the key and its frequency as the value.
+# 4.Create a dictionary, freq_words, that contains each word in string str1 as the key and its frequency as the value.
 
 str1 = "I wish I wish with all my heart to fly with dragons in a land apart"
 
@@ -50,7 +51,7 @@ for word in split_str1:
 print(freq_words)
     
 
-5.Create a dictionary called wrd_d from the string sent, so that the key is a word and the value is how many times you have seen that word.
+# 5.Create a dictionary called wrd_d from the string sent, so that the key is a word and the value is how many times you have seen that word.
 
 sent = "Singing in the rain and playing in the rain are two entirely different situations but both can be good"
 
@@ -65,7 +66,7 @@ for word in split_str1:
 print(wrd_d)
     
 
-6.Create the dictionary characters that shows each character from the string sally and its frequency. Then, find the most frequent letter based on the dictionary. Assign this letter to the variable best_char.
+# 6.Create the dictionary characters that shows each character from the string sally and its frequency. Then, find the most frequent letter based on the dictionary. Assign this letter to the variable best_char.
 
 sally = "sally sells sea shells by the sea shore"
 
@@ -84,7 +85,7 @@ for item in keys:
         
 print("Most frequent letter is: ", best_char)
 
-7.Find the least frequent letter. Create the dictionary characters that shows each character from string sally and its frequency. Then, find the least frequent letter in the string and assign the letter to the variable worst_char.
+# 7.Find the least frequent letter. Create the dictionary characters that shows each character from string sally and its frequency. Then, find the least frequent letter in the string and assign the letter to the variable worst_char.
 
 sally = "sally sells sea shells by the sea shore and by the road"
 
@@ -103,9 +104,9 @@ for item in keys:
         
 print("Least frequent letter is: ", worst_char)
 
-8.Create a dictionary named letter_counts that contains each letter and the number of times it occurs in string1. Challenge: Letters should not be counted separately as upper-case and lower-case. Intead, all of them should be counted as lower-case.
+# 8.Create a dictionary named letter_counts that contains each letter and the number of times it occurs in string1. Challenge: Letters should not be counted separately as upper-case and lower-case. Intead, all of them should be counted as lower-case.
 
-string1 = "There is a tide in the affairs of men, Which taken at the flood, leads on to fortune. Omitted, all the voyage of their life is bound in shallows and in miseries. On such a full sea are we now afloat. And we must take the current when it serves, or lose our ventures."
+string1 = "There is a tide in the affairs of men, Which taken at the flood, leads on to fortune. Omitted, all the voyage of their life is bound in shallows and in miseries. On such a full sea are we now afloat. And we must take the current when it serves, or lose our ventures." #https://github.com/shawonAlam
 
 lower_string1 = string1.lower()
 
@@ -121,7 +122,7 @@ for key in letter_counts:
     print("'",key, "': ", letter_counts[key])
 
 
-9.Create a dictionary called low_d that keeps track of all the characters in the string p and notes how many times each character was seen. Make sure that there are no repeats of characters as keys, such that “T” and “t” are both seen as a “t” for example.
+# 9.Create a dictionary called low_d that keeps track of all the characters in the string p and notes how many times each character was seen. Make sure that there are no repeats of characters as keys, such that “T” and “t” are both seen as a “t” for example. #https://github.com/shawonAlam
 
 p = "Summer is a great time to go outside. You have to be careful of the sun though because of the heat."
 
