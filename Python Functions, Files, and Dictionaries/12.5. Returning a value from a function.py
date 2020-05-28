079519497873563fd52a9bd7a91b49fc63d73754
shawonAlam <30968890+shawonAlam@users.noreply.github.com>
2020-05-29 03:36:22 +0600

    Update 12.5. Returning a value from a function.py

--- Python Functions, Files, and Dictionaries/12.5. Returning a value from a function.py
+++ Python Functions, Files, and Dictionaries/12.5. Returning a value from a function.py
@@ -1,4 +1,4 @@
-8. Write a function named same that takes a string as input, and simply returns that string.
+# 8. Write a function named same that takes a string as input, and simply returns that string.
 
 
 def same(y):
@@ -7,7 +7,7 @@ samme = input("Write something: ")
 print(same(samme))
 
 
-9. Write a function called same_thing that returns the parameter, unchanged.
+# 9. Write a function called same_thing that returns the parameter, unchanged.
 
 def same_thing(y):
     return y
@@ -15,7 +15,7 @@ p = "unchanged"
 print(same_thing(p))
 
 
-10. Write a function called subtract_three that takes an integer or any number as input, and returns that number minus three.
+# 10. Write a function called subtract_three that takes an integer or any number as input, and returns that number minus three.                                                                                     https://github.com/shawonAlam
 
 
 def subtract_three(y):
@@ -26,7 +26,7 @@ item = int(input("Write a number: "))
 print(subtract_three(item))
 
 
-11. Write a function called change that takes one number as its input and returns that number, plus 7.
+# 11. Write a function called change that takes one number as its input and returns that number, plus 7.                                                                                               https://github.com/shawonAlam
 
 
 def change(y):
@@ -36,7 +36,7 @@ def change(y):
 item = int(input("write a number: "))
 print(change(item))
 
-12. Write a function named intro that takes a string as input. Given the string “Becky” as input, the function should return: “Hello, my name is Becky and I love SI 106.”
+# 12. Write a function named intro that takes a string as input. Given the string “Becky” as input, the function should return: “Hello, my name is Becky and I love SI 106.”
 
 
 def intro(name):
@@ -47,7 +47,7 @@ item = str(input("Enter your name: "))
 print(intro(item))
     
 
-13. Write a function called s_change that takes one string as input and returns that string, concatenated with the string ” for fun.”.
+# 13. Write a function called s_change that takes one string as input and returns that string, concatenated with the string ” for fun.”.
 
 
 def s_change(word):
@@ -58,7 +58,7 @@ def s_change(word):
 item = str(input("write something: "))
 print(s_change(item))
 
-14. Write a function called decision that takes a string as input, and then checks the number of characters. If it has over 17 characters, return “This is a long string”, if it is shorter or has 17 characters, return “This is a short string”.
+# 14. Write a function called decision that takes a string as input, and then checks the number of characters. If it has over 17 characters, return “This is a long string”, if it is shorter or has 17 characters, return “This is a short string”. https://github.com/shawonAlam
 
 def decision(value):
     c = len(value)
