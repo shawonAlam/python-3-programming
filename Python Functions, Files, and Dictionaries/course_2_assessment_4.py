72576ea61634678a29de69bc14d385c29848da17
shawonAlam <30968890+shawonAlam@users.noreply.github.com>
2020-05-30 00:17:23 +0600

    Update course_2_assessment_4.py

--- Python Functions, Files, and Dictionaries/course_2_assessment_4.py
+++ Python Functions, Files, and Dictionaries/course_2_assessment_4.py
@@ -53,7 +53,7 @@ print(length([2,4,6,8,4,6,8]))
 
 
 
-# 6.You will need to write two functions for this problem. The first function, divide that takes in any number and returns that same number divided by 2. The second function called sum should take any number, divide it by 2, and add 6. It should return this new number. You should call the divide function within the sum function. Do not worry about decimals.
+# 6.You will need to write two functions for this problem. The first function, divide that takes in any number and returns that same number divided by 2. The second function called sum should take any number, divide it by 2, and add 6. It should return this new number. You should call the divide function within the sum function. Do not worry about decimals. https://github.com/shawonAlam
 
 def divide(x):
     return x/2
